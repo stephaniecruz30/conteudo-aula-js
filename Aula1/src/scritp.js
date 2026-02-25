3fb54c258749f7fc965084296f618e80dbcf5233
Stephanie Almeida <stephaniecruz3019.gmail.com>
2026-02-24 22:14:08 -0300

    Aula 1 - Variáveis em JavaScript

---
+++ Aula1/src/scritp.js
@@ -0,0 +1,11 @@
+// Exemplos de variável let
+let none = 'Richard';
+let none2 = 'Nathalia';
+
+console.log(none);
+console.log(none2);
+
+// Exemplos de variável const
+const times = ['São Paulo','Corinthians', 'Santos'];
+
+console.log(times);
