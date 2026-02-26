eac8d67a9b32d388c6dd0f343357871bcc167a97
Stephanie Almeida <stephaniecruz3019.gmail.com>
2026-02-25 21:19:00 -0300

    Aula 2 - Continuação de Variáveis

---
+++ Aula2/src/scritp.js
@@ -0,0 +1,96 @@
+// Exemplos de operador de atribuição
+let x = 100;
+
+x = x + 5;
+//console.log(x);
+
+x = x - 5;
+//console .log(x);
+
+//Exemplos de Aritmética em JavaScript
+let soma = 5 + 5 + 4;
+//console.log(soma);
+
+//const primeiroNome = 'Stephanie';
+//const ultimoNome = ' Cruz';
+
+let nomeCompleto = "Stephanie" + " " + "Cruz";
+
+//console.log(nomeCompleto);
+
+//exemplos de tipos em JaScript
+//String
+let cor = "Azul";
+let fruta = "Laranja";
+console.log(cor);
+console.log(fruta);
+
+//Number
+let soma1 = 5 + 5;
+let subtracao = 10 - 3;
+console.log(soma1);
+console.log(subtracao);
+
+//Biginint
+let bigint = 1234567890123456789012345n;
+let bigint2 = BigInt(1234567890123456789012345);
+console.log(bigint);
+console.log(bigint2);
+
+//Boolean
+let ValorVerdadeiro = true;
+let ValorFalso = false;
+console.log(ValorVerdadeiro);
+console.log(ValorFalso);
+
+//Object
+const carro = {
+  marca: "Porsche",
+  modelo: "911 Turbo S",
+  cor: "Ruby Star Neo",
+}
+
+console.log(carro);
+
+console.log('A marca do carro é: ' +  carro.marca);
+
+//Array Object
+const frutas = ['laranja', 'Banana','Uva', 'Morango', 'Abacaxi'];
+
+console.log(frutas);
+
+//Date Object
+const data = new Date;
+
+console.log(data);
+
+//Undefined
+let indefinido;
+
+console.log(indefinido);
+
+//Null
+let nulo = null;
+
+console.log(nulo);
+
+//symbol
+let y = Symbol();
+
+console.log(y);
+
+//Exemplos de typeof
+let stringTipo = "300";
+let numberTipo = 300;
+
+//const tipoDado = typeof stringTipo;
+const tipoDado = typeof numberTipo;
+
+console.log(tipoDado);
+
+//Exemplos de Boolean
+let cinco = 5;
+let comparacao = cinco == 8;
+
+console.log(comparacao);
+ 
