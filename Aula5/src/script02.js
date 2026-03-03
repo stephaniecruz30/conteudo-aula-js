99b4981c31ca2271e26c04dc13ced0d02c3db42e
Stephanie Almeida <stephaniecruz3019.gmail.com>
2026-03-02 22:13:44 -0300

    Aula 5 - Laços de Repetição

---
+++ Aula5/src/script02.js
@@ -0,0 +1,12 @@
+//Exemplo de operador NOT
+let usuarioLogado = false;
+
+if (!usuarioLogado){
+    //console.log('teste');
+}
+
+let lista = [];
+
+if (!lista.length) {
+    console.log('A lista está vazia');
+}
