72301975d78d2d82696acd9e6ab9faf87b9dd05c
Stephanie Almeida <stephaniecruz3019.gmail.com>
2026-03-05 22:10:43 -0300

    Aula 7 - Estudando Métodos de String

---
+++ Aula7/src/scritpt02.js
@@ -0,0 +1,50 @@
+// Exemplo de método charAt()
+let texto = "Abacate";
+let exCharAt = texto.charAt(1);
+
+console.log(exCharAt);
+
+// Utilização do método charCodeAt
+let exCharcodeAt = texto.charCodeAt(6);
+console.log(exCharcodeAt);
+
+// Utilização do método codePoinAt()
+let exCodePointAt = texto.codePointAt(6);
+console.log(exCodePointAt);
+
+// Utilização do método at()
+let exAt = texto.at(-2);
+let exAtComoArray = texto[2];
+console.log(exAt);
+console.log(exAtComoArray);
+
+// Utilização do método concat()
+let texto2 = "Verde";
+let texto3 = texto.concat(" ", texto2);
+console.log(texto3);
+
+// Utilização do método slice()
+let frutas = "Banana, Maçã e Uva";
+let exSlice = frutas.slice(2);
+console.log(exSlice);
+
+// Utilização de toUpperCase() e toLowerCase
+let carro = "Gol gti";
+let carro2 = "IMPALA";
+
+let exLowerCase = carro2.toLowerCase();
+let exUpperCase = carro.toUpperCase();
+
+console.log(exLowerCase);
+console.log(exUpperCase);
+
+// Utilização do método iswellFormed()
+let saudacao = "Olá, seja bem-vindo!"
+let exWellFormed = saudacao.isWellFormed();
+
+console.log(exWellFormed);
+
+// Utilização do método trim()
+let thalison = "                 O Thalison gosta do Corinthians                        "
+let exTrim = thalison.trim();
+console.log(exTrim);
