5e35a86075428d9340c09b0570b115b94d23d575
Stephanie Almeida <stephaniecruz3019.gmail.com>
2026-03-06 22:08:55 -0300

    Aula 8 - Continuação dos estudos voltados a métodos deString

---
+++ Aula8/src/scritpt.js
@@ -0,0 +1,79 @@
+// Utilização de método padStart()
+let texto = '7';
+texto = texto.padStart(3, "0");
+
+console.log(texto);
+
+// Utilização do método padEnd()
+let texto2 = "7";
+texto2 = texto2.padEnd(3, "0");
+
+console.log(texto2);
+
+// Utilização do método repeat()
+let fruta = 'Laranja ';
+let repeticao = fruta.repeat(4);
+
+console.log(repeticao);
+
+// Utilização do método replace()
+let carro = 'Impala';
+let novoCarro = carro.replace('Impala', 'Supra');
+
+console.log(novoCarro);
+
+// Utilização do método replaceAll()
+let frase = 'Eu amo gatos. Gatos são muito bons. Gatos dão uma experiência única.';
+
+frase = frase.replaceAll('Gatos', 'Livros');
+frase = frase.replaceAll('gatos','livros');
+
+console.log(frase);
+// Utilização do método split()
+let saudacao = 'Olá, seja bem-vindo';
+let exSplit = saudacao.split(" ");
+
+console.log(exSplit);
+
+// Utilização dos métodos idexOf(),  lastIndexof() e search()
+let exemplo = "Por favor localize onde 'localize' ocorre";
+
+let exIndexOf = exemplo.indexOf('localize');
+
+let exLastIndexOf = exemplo.lastIndexOf('localize');
+
+let exSearch = exemplo.search('favor');
+
+console.log(exIndexOf);
+console.log(exLastIndexOf);
+console.log(exSearch);
+
+// Utilização do método includes()
+let olaMundo = 'Olá, mundo. Bem-vindo ao universo.';
+let exIncludes = olaMundo.includes('mundo');
+
+console.log(exIncludes);
+
+// Utilização do método startsWith()
+let olaMundo2 = 'Olá, mundo. Bem-vindo ao universo.';
+let exStartsWith = olaMundo2.startsWith('olá'); 
+let exStartsWith2 = olaMundo2.startsWith('universo'); 
+
+console.log(exStartsWith);
+console.log(exStartsWith2);
+
+// Utilização do método endWith()
+let nome = 'Stephanie Almeida';
+let exEndesWith = nome.endsWith('Almeida');
+
+console.log(exEndesWith);
+
+
+
+
+
+
+
+
+
+
