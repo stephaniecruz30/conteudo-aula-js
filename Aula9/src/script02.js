c1f0056f362bd48b5965aea1b4ea752c749efa7d
Stephanie Almeida <stephaniecruz3019.gmail.com>
2026-03-09 20:22:20 -0300

    Aula 9 - Propriedades de Number em JavaScript

---
+++ Aula9/src/script02.js
@@ -0,0 +1,38 @@
+// Utilização da propriedade EPSILON
+let a =Number.EPSILON;
+console.log(a);
+
+// Utilização da propriedade MAX-VALUE
+let b = Number.MAX_VALUE;
+console.log(b);
+
+// Utilização da propriedade MIN_VALUE
+let c = Number.MIN_VALUE;
+console.log(c);
+
+// Utilização da propriedade MIN_SAFE_INTEGER
+let d = Number.MIN_SAFE_INTEGER;
+console.log(d);
+
+// Utilização da propriedade MAX_SAFE_INTEGER
+let e = Number.MIN_SAFE_INTEGER;
+console.log(e);
+
+// Utilização da propriedade POSITIVE_INFINITY
+let f = Number.POSITIVE_INFINITY;
+console.log(f);
+
+// Utilização da propriedade NEGATIVE_INFINITY
+let g = Number.NEGATIVE_INFINITY;
+console.log(g);
+
+// Utilização da propriedade NAN
+let h = Number.NaN;
+console.log(h);
+
+
+
+
+
+
+
