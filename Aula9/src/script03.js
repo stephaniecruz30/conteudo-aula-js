f6c7d066fcace4208c584ce7eedfe6c91f9379c5
Stephanie Almeida <stephaniecruz3019.gmail.com>
2026-03-09 21:12:40 -0300

    Aula 9 - Aprendendo BigInt em JavaScript

---
+++ Aula9/src/script03.js
@@ -0,0 +1,35 @@
+// Utilizando o BigInt
+let a = BigInt
+("2369458306867463527131462743278875089786785745473193578");
+console.log(a);
+
+// Utizando n
+let b = 11204792579385938538539093950239859867349868679834769834769786594876948357935n;
+console.log(b);
+
+// Utilizando operadores aritméticos com BigInt
+let c = 11131245366860384893890n;
+let d = 11131245366860384893890n;
+let e = (c * d);
+console.log(e);
+
+// Convertendo BigInt
+let f = BigInt("39539006450387644986486984769390");
+let fConvertido = Number(f);
+console.log(fConvertido);
+
+// Utilizando métodos de comparação em BigInt
+let g = 100n;
+let h = 90n;
+let exComparacao = (g > h);
+console.log(exComparacao);
+
+
+
+
+
+
+
+
+
+
