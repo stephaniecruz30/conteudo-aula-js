5ba861cf0e7dfa6355808c2143f5d677d7357093
Stephanie Almeida <stephaniecruz3019.gmail.com>
2026-03-10 21:23:00 -0300

    Aula 10 - Finalização de Funções

---
+++ Aula10/src/scritpt.js
@@ -0,0 +1,20 @@
+// Definição padrão de função
+function soma(a, b) {
+  return a + b;
+}
+
+let a = soma(30, 70);
+console.log(a);
+
+// Função armazenada em variável
+const multiplicar = function (a, b) {return a * b};
+  
+
+let b = multiplicar(3, 4);
+console.log(b);
+
+// Arrow Function (Função de Seta)
+let subtracao = (a, b) => a - b;
+
+let c = subtracao(5, 2);
+console.log(c);
