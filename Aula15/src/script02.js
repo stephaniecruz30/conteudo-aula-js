d3f26eebfe3a626cf8a8f1fc8b5dfcc6c2b29a2e
Stephanie Almeida <stephaniecruz3019.gmail.com>
2026-03-18 21:22:21 -0300

    Aula 15 - Estudando o DOM

---
+++ Aula15/src/script02.js
@@ -0,0 +1,36 @@
+// Utilizando o getElementById()
+const elemento1 = document.getElementById("selecionandoElemento");
+
+console.log(elemento1);
+
+// Utilizando o getElementByTagName()
+const elemento2 = document.getElementsByTagName("h1");
+
+console.log(elemento2[0]);
+
+// Utilizando getElementsByClassName()
+const elemento3 = document.getElementsByClassName("selecionandoElementoPorClasse");
+
+console.log(elemento3);
+
+// Utilizando o querySelector()
+const elemento4 = document.querySelector(".elementoClasse");
+
+console.log(elemento4);
+
+const elemento5 = document.querySelector("#elementoID");
+
+
+console.log(elemento5);
+
+// Utilizando o querySelectoAll()
+const elementos = document.querySelectorAll("h3");
+
+console.log(elementos[0]);
+
+// 
+
+
+
+
+
