68428f438686a59d3b3e613c50a1f6aafc3672ea
Stephanie Almeida <stephaniecruz3019.gmail.com>
2026-03-19 21:27:22 -0300

    Aula 16 - Estudando Validações em formulários

---
+++ Aula16/src/script03.js
@@ -0,0 +1,9 @@
+// Validação de Formulário
+
+function validarForm() {  
+    let formulario = document.forms["novoForm"]["texto"].value;
+    if (formulario == "") {
+        alert("Por favor, insira um valor para o campo!");
+        return false;
+    } 
+}
