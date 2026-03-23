cbdfe28c977b91c1bd80a58b2f4ee7962c8df5ec
Stephanie Almeida <stephaniecruz3019.gmail.com>
2026-03-23 19:26:57 -0300

    Aula 18 - Estudando eventos de carregamento

---
+++ Aula18/src/script.js
@@ -0,0 +1,12 @@
+// Utilizando o evento de carregamento load
+const img = document.querySelector(".minha-imagem");
+
+
+img.addEventListener("load", function (){
+  const elemento = document.querySelector(".info-carregamento");
+  elemento.innerHTML = "A imagem foi carregada.";
+});
+
+
+
+
